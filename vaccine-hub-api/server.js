36af70b7761cce2f57569d861b67eadddb0ed0a9
Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>
2023-06-26 16:09:20 -0700

    created skeleton

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const { BadRequestError, NotFoundError } = require("./utils/errors")
+
 const app = express();
 
 //enables cross-origin resource
@@ -12,6 +14,23 @@ app.use(express.json());
 
 //log data
 app.use(morgan("tiny"));
+
+
+app.use((req, res, next) => {
+    return next (new NotFoundError());
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: {message, status},
+    })
+
+})
+
+
 
 const PORT = process.env.PORT || 3001;
 
